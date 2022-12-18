d7e04a9e34582498180af1d988348d75e46c3297
blakemason.dev <blakemason.dev@gmail.com>
2022-12-19 06:41:12 +0800

    added radio buttons

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Phaser from 'phaser';
 import { PlayGame } from "../game/scenes/main";
 import Button from "./Button";
 
 import './Game.css';
+import Radio from "./Radio";
 
 const Game = () => {
     const initUseEffectCalled = useRef(false);
+
+    const [shape, setShape] = useState('circle');
 
     useEffect(() => {
         if (initUseEffectCalled.current) return;
@@ -19,20 +22,45 @@ const Game = () => {
             parent: 'game',
             scene: PlayGame
         }
-    
+
         const game = new Phaser.Game(config);
         console.log('Create new Phaser Game');
 
     }, [])
 
-    
-
     return (
-        <>
-            <Button />
-            <div id="game" className="game">
+        <div className="game">
+            <form className="game__config">
+                <input
+                    type="radio"
+                    id="circle"
+                    checked={shape === 'circle'}
+                    onClick={() => setShape("circle")}
+                />
+                <label>Circle</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="square"
+                    checked={shape === 'square'}
+                    onClick={() => setShape("square")}
+                />
+                <label>Square</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="triangle"
+                    checked={shape === 'triangle'}
+                    onClick={() => setShape("triangle")}
+                />
+                <label>Triangle</label>
+                <br />
+            </form>
+            <div id="game" className="game__viewer">
             </div>
-        </>
+        </div>
     )
 }
 
